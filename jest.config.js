1f8a2ffe286bd4ce7347d9735d1618d8c538da15
thalles <thallesgabriel1307@gmail.com>
2022-10-25 14:13:09 -0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+const { resolve } = require('path')
+
+module.exports = {
+  rootDir: resolve(__dirname),
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/app/contracts/**',
+    '!<rootDir>/src/domain/**',
+    '!<rootDir>/src/presentation/protocols/**',
+    '!<rootDir>/src/shared/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  clearMocks: true,
+  preset: 'ts-jest',
+  setupFiles: ['./jest-setup-before-env.js'],
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '@tests/(.*)': '<rootDir>/tests/$1'
+  }
+}
